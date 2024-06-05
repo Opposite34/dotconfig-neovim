819ee9a1a5f2dc926e25d86ee0149969ab68e271
Opposite34 <54463205+Opposite34@users.noreply.github.com>
2024-06-06 01:53:51 +0900

    change lua line mode's color a bit to fit my overall setup

--- lua/plugins/theming.lua
+++ lua/plugins/theming.lua
@@ -25,8 +25,22 @@ return {
     priority = 900,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+      local custom_palenight = require("lualine.themes.palenight")
+
+      custom_palenight.normal.a.bg = "#f38ba8"
+      custom_palenight.normal.b.fg = "#f38ba8"
+
+      custom_palenight.insert.a.bg = "#89dceb"
+      custom_palenight.insert.b.fg = "#89dceb"
+
+      custom_palenight.visual.a.bg = "#cba6f7"
+      custom_palenight.visual.b.fg = "#cba6f7"
+
+      custom_palenight.replace.a.bg = "#94e2d5"
+      custom_palenight.replace.b.fg = "#94e2d5"
+
       require('lualine').setup({
-        options = { theme = 'palenight' }
+        options = { theme = custom_palenight }
       })
     end,
    },
